6d9b56ccacdf55f37f73bc0319e0924331b07ae9
bard-regard <29786941+dirty-kebab@users.noreply.github.com>
2025-04-07 07:43:44 +0100

    add logging, add detailed error messaging, rename variables and parameters, add extensive error handling

--- api/api.go
+++ api/api.go
@@ -4,13 +4,13 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/Masterminds/semver/v3"
+	"github.com/gorilla/mux"
+	"github.com/snyk/snyk-code-review-exercise/utilities"
 	"io"
 	"log"
 	"net/http"
 	"sort"
-
-	"github.com/Masterminds/semver/v3"
-	"github.com/gorilla/mux"
 )
 
 func New() http.Handler {
@@ -40,100 +40,94 @@ type NpmPackageVersion struct {
 
 func packageHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	// review: we should validate and sanitise our inputs, especially if they will be sent to other services in the future
 	pkgName := vars["package"]
-	pkgVersion := vars["version"]
+	pkgVersion := vars["version"] // validated by semver package
 
-	rootPkg := &NpmPackageVersion{Name: pkgName, Dependencies: map[string]*NpmPackageVersion{}}
-	if err := resolveDependencies(rootPkg, pkgVersion); err != nil {
-		// review: use logging - timestamps, log levels, stderr, formatting, extensible - best for observability
-		println(err.Error())
-		// review: magic number - http.StatusInternalServerError
-		w.WriteHeader(500)
+	if err := utilities.Validate(pkgName); err != nil {
+		log.Printf("Error validating package name: %v", err)
+		w.WriteHeader(http.StatusUnprocessableEntity)
 		return
 	}
 
-	// review: consider more appropriate name, like `res` or `out`
-	// review: use json.Marshal(rootPkg) for prod - reduces payload size - keep Indent for dev maybe
-	// review 2: However, this can stay as its AC #2.
-	stringified, err := json.MarshalIndent(rootPkg, "", "  ")
+	log.Printf("Package: %s, Version: %s", pkgName, pkgVersion)
+
+	rootPkg := &NpmPackageVersion{Name: pkgName, Dependencies: map[string]*NpmPackageVersion{}}
+	if err := resolveDependencies(rootPkg, pkgVersion); err != nil {
+		log.Printf("Error resolving dependencies: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	output, err := json.MarshalIndent(rootPkg, "", "  ")
 	if err != nil {
-		// review: use logging - timestamps, log levels, stderr, formatting, extensible - best for observability
-		println(err.Error())
-		// review: magic number - http.StatusInternalServerError
-		w.WriteHeader(500)
+		log.Printf("Error marshalling dependency tree: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	// idea: consider adding gzip, we may be sending a lot of information and this would save on bandwidth and latency
 	w.Header().Set("Content-Type", "application/json")
-	// review: magic number - http.StatusOk
-	w.WriteHeader(200)
+	w.WriteHeader(http.StatusOK)
 
 	// review: ignoring errors from `w.Write` could hide failed responses like when a client disconnects
-	// Ignoring ResponseWriter errors
-	_, _ = w.Write(stringified)
-
-	// idea: add logging for successful result
+	outStatus, err := w.Write(output)
+	if err != nil || outStatus != http.StatusOK {
+		log.Printf("Error writing response: %v %v", outStatus, err)
+		return
+	}
 }
 
 // review: inconsistent variable nomenclature, consider using `ver` or `version` instead of `versionConstraint` as it's overly specific
 // idea: recursion can cause stack overflows, consider a linear approach
 // review: this is slow, consider using a worker pool & mutex/atomics
-func resolveDependencies(pkg *NpmPackageVersion, versionConstraint string) error {
+func resolveDependencies(pkg *NpmPackageVersion, version string) error {
 	pkgMeta, err := fetchPackageMeta(pkg.Name)
 	if err != nil {
-		// review: Add logging & wrapping: 'failed to fetch package metadata for <pkg.Name>'
-		return err
+		return fmt.Errorf("failed to fetch package metadata for <%s,%s>: %v", pkg.Name, pkg.Version, err)
 	}
-	concreteVersion, err := highestCompatibleVersion(versionConstraint, pkgMeta)
+
+	if pkgMeta == nil {
+		return fmt.Errorf("failed to fetch package metadata (nil pointer) for <%s,%s>", pkg.Name, pkg.Version)
+	}
+
+	pkg.Version, err = highestCompatibleVersion(version, pkgMeta)
 	if err != nil {
-		// review: Add logging & wrapping: 'failed to resolve version for <pkg.Name> with constraint <versionConstraint>'
-		return err
+		return fmt.Errorf("failed to resolve compatible existing version for <%s,%s>: %v", pkg.Name, pkg.Version, err)
 	}
-	// review: wasted variable, just initially assign pkg.Version
-	pkg.Version = concreteVersion
 
 	// review: because of `npmPkg`, `pkg.Dependencies` is never used, so it's a wasted variable
 	npmPkg, err := fetchPackage(pkg.Name, pkg.Version)
 	if err != nil {
-		// review: Add logging & wrapping: 'failed to fetch package dependencies <pkg.Version> <pkg.Name>'
-		// this will help especially with deep recursion
-		return err
+		return fmt.Errorf("failed to fetch package dependencies for <%s,%s>: %v", pkg.Name, pkg.Version, err)
 	}
+
 	// review: our code does nothing to prevent circular dependencies
 	// review: simplify variable names `depName, depVer` Go style guides support this as this will be clear enough
-	for dependencyName, dependencyVersionConstraint := range npmPkg.Dependencies {
-		dep := &NpmPackageVersion{Name: dependencyName, Dependencies: map[string]*NpmPackageVersion{}}
-		pkg.Dependencies[dependencyName] = dep
-		// review: shadowed err variable, use another name like `err2` to improve readability and prevent bugs
-		if err := resolveDependencies(dep, dependencyVersionConstraint); err != nil {
-			// review: Add logging & wrapping: 'failed to resolve dependencies for <pkg.Version> <pkg.Name>'
-			// this will help especially with deep recursion
-			return err
+	for name, v := range npmPkg.Dependencies {
+		dep := &NpmPackageVersion{Name: name, Dependencies: map[string]*NpmPackageVersion{}}
+		pkg.Dependencies[name] = dep
+		if err = resolveDependencies(dep, v); err != nil {
+			return fmt.Errorf("failed to resolve dependencies for <%s,%s>: %v", pkg.Name, pkg.Version, err)
 		}
 	}
 	return nil
 }
 
-// review: `versions` is Package Metadata, and has no conflicting names within this scope, this inconsistency increases reading time
-// review: rename both parameters `version string, pkgMeta *npm...`
-func highestCompatibleVersion(constraintStr string, versions *npmPackageMetaResponse) (string, error) {
-	constraint, err := semver.NewConstraint(constraintStr)
+func highestCompatibleVersion(version string, pkgMeta *npmPackageMetaResponse) (string, error) {
+	constraint, err := semver.NewConstraint(version)
 	if err != nil {
 		return "", err
 	}
-	filtered := filterCompatibleVersions(constraint, versions)
+	filtered := filterCompatibleVersions(constraint, pkgMeta)
 	sort.Sort(filtered)
 	if len(filtered) == 0 {
-		return "", errors.New("no compatible versions found")
+		return "", errors.New("no compatible version found")
 	}
 	return filtered[len(filtered)-1].String(), nil
 }
 
 func filterCompatibleVersions(constraint *semver.Constraints, pkgMeta *npmPackageMetaResponse) semver.Collection {
 	var compatible semver.Collection
-	// review: between fetchPackageMeta and here, there are no nil pointer checks on pkgMeta
 	for version := range pkgMeta.Versions {
 		semVer, err := semver.NewVersion(version)
 		if err != nil {
@@ -151,7 +145,13 @@ func fetchPackage(name, version string) (*npmPackageResponse, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
+	defer func(Body io.ReadCloser) {
+		errDef := Body.Close()
+		if errDef != nil {
+			log.Printf("Error closing body while fetching package: %v", err)
+			return
+		}
+	}(resp.Body)
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
@@ -159,8 +159,12 @@ func fetchPackage(name, version string) (*npmPackageResponse, error) {
 	}
 
 	var parsed npmPackageResponse
-	// review: ignored error could cause undocumented bugs
-	_ = json.Unmarshal(body, &parsed)
+
+	err = json.Unmarshal(body, &parsed)
+	if err != nil {
+		return nil, fmt.Errorf("error during package unmarshal: %v", err)
+	}
+
 	return &parsed, nil
 }
 
@@ -178,18 +182,21 @@ func fetchPackageMeta(p string) (*npmPackageMetaResponse, error) {
 	// review 3: so, the most optimal thing to do is, fetch the abbreviated metadata,
 	// then use that to find the latest semantic version, which has the dependencies included already,
 	// -> no need for a double fetch per package
-	url := fmt.Sprintf("https://registry.npmjs.org/%s", p)
-	resp, err := http.Get(url)
-	log.Printf("url: %s", url)
-	log.Printf("resp: %s", resp)
-	// review: `err != nil || resp.Status != http.StatusOK`
-	if err != nil {
-		log.Printf("err: %s", err)
-		// review: Add logging & wrapping: 'failed to resolve version for <pkg.Name> with constraint <versionConstraint>'
-		return nil, err
+	req, _ := http.NewRequest("GET", fmt.Sprintf("https://registry.npmjs.org/%s", p), nil)
+	req.Header.Set("Accept", "application/vnd.npm.install-v1+json")
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil || resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to fetch package metadata for <%s>: %v", p, err)
 	}
 	// review: Add error handling to the deferred close
-	defer resp.Body.Close()
+	defer func(Body io.ReadCloser) {
+		errDef := Body.Close()
+		if errDef != nil {
+			log.Printf("Error closing body wihle fetching metadata: %v", err)
+			return
+		}
+	}(resp.Body)
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
@@ -197,11 +204,8 @@ func fetchPackageMeta(p string) (*npmPackageMetaResponse, error) {
 	}
 
 	var parsed npmPackageMetaResponse
-	// review: type case `[]byte()` is redundant, `body` is already a Byte array
-	// review 2: shadowed error, rename to `err2` or anything else
-	// idea: consider using streaming decoder `json.NewDecoder(resp.Body)` for lower memory usage
-	if err := json.Unmarshal([]byte(body), &parsed); err != nil {
-		return nil, err
+	if err = json.Unmarshal(body, &parsed); err != nil {
+		return nil, fmt.Errorf("error during metadata unmarshal: %v", err)
 	}
 
 	return &parsed, nil
